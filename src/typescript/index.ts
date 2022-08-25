1a2a1ccb61a85b0f481c4753d9e4cd4e7470e370
jingzhi.wang <jingzhi.wang@eeoa.com>
2022-08-25 20:25:17 +0800

    feat: 更新ts实现一个单例模式

--- src/typescript/index.ts
+++ src/typescript/index.ts
@@ -1,5 +1,5 @@
 /**
- * const func = (a, b) => a + b; 要求编写Typescript，要求a，b参数类型一致，都为number或者都为string
+ * 1. const func = (a, b) => a + b; 要求编写Typescript，要求a，b参数类型一致，都为number或者都为string
  */
 // 方法一：函数重载
 function func1(a:number,b:number):number
@@ -9,7 +9,7 @@ function func1(a: any, b: any):any{
 }
 console.log('func1:',func1(1,2))
 
-// 方法二：
+// 方法二：函数声明
 interface Add {
   (a:number,b:number):number;
   (a:string,b:string):string;
@@ -19,3 +19,26 @@ function func2(a,b):Add{
 }
 
 console.log('func2:',func1(1,2))
+
+/**
+ * 2. 用ts实现一个单例模式
+ */
+
+class MySingleton1 {
+  static singleton: MySingleton1
+  private constructor(public name){
+    this.name = name
+    console.log('MySingleton1被实例化了')
+  }
+  public static getInstance(name:string){
+    if(this.singleton) {
+      return this.singleton
+    }
+    return this.singleton = new MySingleton1(name)
+  }
+}
+const singleton1 = MySingleton1.getInstance('wang')
+const singleton2 = MySingleton1.getInstance('frank')
+console.log(singleton1.name)
+console.log(singleton2.name)
+console.log(singleton1 === singleton2)
