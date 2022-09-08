72e9813b98ad16865a162cc8d30c50c23f28ce88
jingzhi.wang <jingzhi.wang@eeoa.com>
2022-09-08 10:52:02 +0800

    feat: 补充注释

--- src/algorithm/debounce/debounce.ts
+++ src/algorithm/debounce/debounce.ts
@@ -8,7 +8,9 @@ export function debounce(fn: Function, wait: number){
   return function(){
     const context = this
     const args = arguments
+    // 每次调用函数前都要先清除之前调用的计时器
     timer && clearTimeout(timer)
+    // 重新开始计时
     timer = setTimeout(()=>{
       fn.apply(context, args)
     }, wait)
