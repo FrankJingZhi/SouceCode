1abe4e021eb63c7e7354a628e3e9d98d87ea1d11
jingzhi.wang <jingzhi.wang@eeoa.com>
2023-04-26 13:25:23 +0800

    fix: 环形链表 - 找出环的起始位置

--- src/algorithm/listNode/circularListNode.ts
+++ src/algorithm/listNode/circularListNode.ts
@@ -37,3 +37,50 @@ list.next.next.next.next.next = new ListNode(6)
 list.next.next.next.next.next.next = list
 
 console.log(circularListNode(list))
+
+
+/**
+ * 环形链表衍生问题——定位环的起点
+ * 
+ * 真题描述：给定一个链表，返回链表开始入环的第一个结点。 如果链表无环，则返回 null。
+
+    示例 1：
+    输入：head = [3,2,0,-4]（如下图） 输出：tail connects to node index 1 解释：链表中有一个环，其尾部连接到第二个结点。
+ */
+
+function startCircularListNode(list){
+  let head = list
+  let temp,
+      index = 0
+  while(list){
+      if(list.flag){
+          temp = list
+          break
+      }else{
+          list.flag = true
+          list = list.next
+      }
+  }
+  if(temp){
+      while(head){
+          if(head === temp){
+              return index
+          }else{
+              index++
+              head = head.next
+          }
+      }
+  }else{
+      return false
+  }
+}
+
+const list1 = new ListNode(1)
+list1.next = new ListNode(2)
+list1.next.next = new ListNode(3)
+list1.next.next.next = new ListNode(4)
+list1.next.next.next.next = new ListNode(5)
+list1.next.next.next.next.next = new ListNode(6)
+list1.next.next.next.next.next.next = list1
+
+console.log(startCircularListNode(list1))
